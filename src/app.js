4d363693e4bea823d3cefafe9129a8547fcdd9b6
Samuel Nogueira <samuel.nogueira716@gmail.com>
2024-03-06 12:04:20 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ db.once("open", ()=> {
 const app = express();
 
 app.options("*", cors({
-        origin: "http://localhost:3000",
+        origin: "*",
         optionsSuccessStatus: 200,
         preflightContinue: true,
         credentials: true,
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(express.json()); 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, token"
